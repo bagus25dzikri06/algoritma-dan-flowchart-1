21fc13470fcd9223d3939cfef2ec20b2b2edba2b
bagus25dzikri06 <bagusdzikri@gmail.com>
2022-07-26 22:23:26 +0700

    Task 2 (Reverse Words) with adding the data type validation in task 1 and 2

--- task1.js
+++ task1.js
@@ -1,9 +1,20 @@
 const palindrome = (word) => {
-  let kataPalindrom = []
-  for (let i = word.length - 1; i >= 0; i--) {
-    kataPalindrom.push(word[i])
+  if (typeof word === 'string' === true) {
+    let kataPalindrom = []
+    for (let i = word.length - 1; i >= 0; i--) {
+      kataPalindrom.push(word[i])
+    }
+
+    if (word.toLowerCase() === kataPalindrom.join('').toLowerCase()) {
+      return 'Palindrom'
+    } else {
+      return 'Bukan Palindrom'
+    } 
+  } else {
+    return 'Input harus berupa string'
   }
-  return (word.toLowerCase() === kataPalindrom.join('').toLowerCase()) ? 'Palindrom' : 'Bukan palindrom'
 }
 
 console.log(palindrome('Malam'))
+console.log(palindrome(828))
+console.log(palindrome('828'))
--- task2.js
+++ task2.js
@@ -1,14 +1,20 @@
 const balikKalimat = (kalimat) => {
-  let kata = kalimat.split(' '), kalimatTerbalik = []
-  if (kata.length === 1) {
-    return kalimat
+  if (typeof kalimat === 'string' === true) {
+    let kata = kalimat.split(' '), kalimatTerbalik = []
+    if (kata.length === 1) {
+      return kalimat
+    } else {
+      for (let i = kata.length - 1; i >= 0; i--) {
+        kalimatTerbalik.push(kata[i])
+      }
+      return kalimatTerbalik.join(' ')
+    }  
   } else {
-    for (let i = kata.length - 1; i >= 0; i--) {
-      kalimatTerbalik.push(kata[i])
-    }
-    return kalimatTerbalik.join(' ')
-  } 
+    return 'Input harus berupa string'
+  }
 }
 
 console.log(balikKalimat('Aku belajar Javascript'))
 console.log(balikKalimat('Javascript'))
+console.log(balikKalimat(1627238))
+console.log(balikKalimat('1627238 4838490'))
